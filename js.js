b8c43eff914936c4367a194f486f6a08ec95cc27
Ahd <90315291+Ahdyahia@users.noreply.github.com>
2022-12-25 14:33:15 +0300

    upload my-portfolio files

---
+++ js.js
@@ -0,0 +1,5 @@
+function changContent(){
+						
+	document.getElementById("aa").innerHTML = "<p> Hi! my name is Ahd Haj Yahia, I'm 23 years old, I'm a fresh graduated from the Faculty of Informatics engineer, I started my journy with front-end since three mounths ago hoping to build a career with it. </p>";
+}
+
